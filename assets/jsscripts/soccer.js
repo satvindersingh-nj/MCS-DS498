b70bf9666c4065a105548a922dabeab32b99f599
satvindersingh <48099428+satwsing66@users.noreply.github.com>
2020-07-31 16:57:13 -0400

    1.8.0.0

--- assets/jsscripts/soccer.js
+++ assets/jsscripts/soccer.js
@@ -45,15 +45,32 @@ async function visualization_1(margin, width, height, animation) {
                 .tickValues([1969, 1972, 1975, 1978, 1981, 1984, 1987, 1990, 1993, 1996, 1999, 2002, 2005, 2008, 2011, 2014, 2017, 2019]))
             .selectAll("text")
             .style("font-size", 14);
+        svg.append("text")
+            .attr("x", x(2016))
+            .attr("y", y(21.5))
+            .attr("opacity", "1")
+            .style("font-weight", "bold")
+            .attr("class", "annot")
+            .style("font-size", 16)
+            .text("Season");
 
         // Add Y axis
         svg.append("g")
             .call(d3.axisLeft(y).tickValues([19, 17, 15, 13, 11, 9, 7, 5, 4, 3, 1]));
+        svg.append("text")
+            .attr("x", x(3))
+            .attr("y", y(1))
+            .attr("opacity", "1")
+            .attr("class", "annot")
+            .style("font-size", 16)
+            .style("font-weight", "bold")
+            .attr("transform", "translate(-35,60) rotate(-90)")
+            .text("Position");
 
         DrawVisualization_1(svg, data, duration, delay, width, height, x, y);
     });
 };
-// Scene 1 - Initializes 
+// Scene 1 - Initialization 
 function DrawVisualization_1(svg, data, duration, delay, width, height, xDomain, yDomain) {
 
     svg.append("g")
@@ -195,13 +212,10 @@ async function visualization_2(margin, width, height, animation) {
         DrawVisualization_2(svg, data, duration, delay, width, height, x, y);
     });
 };
-// Scene 2 - Initializes
+// Scene 2 - Initialization
 function DrawVisualization_2(svg, data, duration, delay, width, height, xDomain, yDomain) {
 
     data = data.filter(function (d) { return (d.year < 1993); });
-
-    line_plot(svg, data, duration, delay, height, xDomain, yDomain);   
-    dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
 
     // Line to indicate team has automatically qualified for the Champions and Europa Leagues
     line(svg, 0, 118, 1300, 118, 0.4, duration, delay);
@@ -210,13 +224,17 @@ function DrawVisualization_2(svg, data, duration, delay, width, height, xDomain,
     // Vertical line to mark beginning of Alex Ferguson Era
     line(svg, xDomain(1992), height, xDomain(1992), 0, 0.7, duration, delay);
 
+    line_plot(svg, data, duration, delay, height, xDomain, yDomain);   
+    dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
+
     // Horizontal lines
-    position_text(svg, 627, 115, "Automatic qualification for Champions League.", 0, 300);
-    position_text(svg, 627, 135, "Automatic qualification for Europa League.", 0, 300);
+    position_text(svg, 627, 115, "Automatic qualification for Champions League.", duration, delay);
+    position_text(svg, 627, 135, "Automatic qualification for Europa League.", duration, delay);
 
     // Annotation using custom annot_line and position_text functions
-    annot_line(svg, xDomain(1992), 318, xDomain(1994), 311, duration, delay);
-    position_text(svg, xDomain(1995), 310, "Sir Alex Ferguson takes over as Manager", duration, delay + 300);
+    annot_line(svg, xDomain(1992)-5, yDomain(1)-5, xDomain(1990), yDomain(-1), duration, delay);
+    position_text(svg, xDomain(1985), yDomain(-1), "Sir Alex Ferguson ", duration, delay);
+    position_text(svg, xDomain(1985), yDomain(-1)+15, "takes over as Manager", duration, delay);
 }
 
 // Scene 3 - Preparing the Scene
@@ -275,18 +293,10 @@ async function visualization_3(margin, width, height, animation) {
         DrawVisualization_3(svg, data, duration, delay, width, height, x, y)
     });
 }
-
+// Scene 3 - Initialization
 function DrawVisualization_3(svg, data, duration, delay, width, height, xDomain, yDomain) {
-
+    // Filter the data for period between 1991 and 2013
     data = data.filter(function (d) { return ((d.year > 1991) && (d.year < 2013)); });
-
-    line_plot(svg, data, duration, delay, height, xDomain, yDomain);
-
-    // Initialize the tooltip
-    var tooltip = d3.select("#tooltip")
-        .style("opacity", 0);
-
-    dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
 
     // Line to indicate team has automatically qualified for the Champions and Europa Leagues
     line(svg, 0, 118, 1300, 118, 0.4, duration, delay);
@@ -295,15 +305,17 @@ function DrawVisualization_3(svg, data, duration, delay, width, height, xDomain,
     // Vertical line to mark end of Alex Ferguson Era
     line(svg, xDomain(2012), height, xDomain(2012), 0, 0.7, duration, delay);
 
-    // Horizontal lines
-    //position_text(svg, 527, 115, "Automatic qualification for Champions League.", duration, delay);
-    //position_text(svg, 527, 135, "Automatic qualification for Europa League.", duration, delay);
+    line_plot(svg, data, duration, delay, height, xDomain, yDomain);
+
+    dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
 
     // Annotation using custom annot_line and position_text functions
-    annot_line(svg, xDomain(2012), 325, xDomain(2010), 347, duration, delay);
-    position_text(svg, xDomain(2002), 352, "Sir Alex Ferguson resigns as Manager", duration, delay + 300);
+    annot_line(svg, xDomain(2012), yDomain(1) - 5, xDomain(2010), yDomain(-1), duration, delay);
+    position_text(svg, xDomain(2006), yDomain(-1), "Sir Alex Ferguson", duration, delay);
+    position_text(svg, xDomain(2006), yDomain(-1)+15, "resigns as Manager", duration, delay);
 };
 
+// Scene 4 - Preparing the Scene
 async function visualization_4(margin, width, height, animation) {
     console.log("visual-4");
     var x = d3.scaleBand().range([0, width]);
@@ -360,12 +372,111 @@ async function visualization_4(margin, width, height, animation) {
         DrawVisualization_4(svg, data, duration, delay, width, height, x, y)
     });
 };
-
+// Scene 4 - Initialization
 function DrawVisualization_4(svg, data, duration, delay, width, height, xDomain, yDomain) {
     data = data.filter(function (d) { return (d.year > 2011); });
 
     line_plot(svg, data, duration, delay, height, xDomain, yDomain);
     dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
+}
+
+// Scene 5 - Preparing the Scene
+async function visualization_5(margin, width, height, animation) {
+    console.log("visual-5");
+    var x = d3.scaleBand().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var duration = (animation == true) ? 3000 : 0;
+    var delay = (animation == true) ? 500 : 0;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    await d3.csv("https://satvindersingh-nj.github.io/MCS-DS498/data/data2.csv", function (error, data) {
+        //await d3.csv("http://localhost/data/data2.csv", function (error, data) {
+
+        if (error) throw error;
+
+        data.forEach(function (d) {
+            d.year = d.year;
+            d.winner = +d.winner;
+            d.cup = +d.cup;
+            d.league = d.desc;
+            d.points = +d.points;
+            d.GF = d.GF;
+            d.GA = d.GA;
+            d.pos = +d.pos;
+            d.manager = d.manager;
+            d.country = d.country;
+        });
+
+        // Define the domain
+        x.domain(data.map(function (d) { return d.year; }));
+        y.domain([22, -2]);
+
+        // Add the X-axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x)
+                .tickValues([1969, 1972, 1975, 1978, 1981, 1984, 1987, 1990, 1993, 1996, 1999, 2002, 2005, 2008, 2011, 2014, 2017, 2019]))
+            .selectAll("text")
+            .style("font-size", 14);
+
+        // Add Y axis
+        svg.append("g")
+            .call(d3.axisLeft(y).tickValues([19, 17, 15, 13, 11, 9, 7, 5, 4, 3, 1]));
+
+        DrawVisualization_5(svg, data, duration, delay, width, height, x, y)
+    });
+};
+
+// Scene 5 - Initialization
+function DrawVisualization_5(svg, data, duration, delay, width, height, xDomain, yDomain) {
+
+    svg.append("rect")
+        .attr("x", xDomain(1992))
+        .attr("y", yDomain(-2))
+        .attr("width", xDomain(2012) - xDomain(1992))
+        .attr("height", yDomain(22))
+        .attr("transform", "translate(12,0)")
+        .style("fill", "#ff0000")
+        .attr("opacity", "0.1");
+
+    line_plot(svg, data, duration, delay, height, xDomain, yDomain);
+    dot_plot(svg, data, duration, delay, height, xDomain, yDomain);
+
+    // Line to indicate team has automatically qualified for the Champions and Europa Leagues
+    line(svg, 0, 118, 1300, 118, 0.4, duration, delay);
+    line(svg, 0, 138, 1300, 138, 0.4, duration, delay);
+
+    // Vertical line to mark beginning of Alex Ferguson Era
+    line(svg, xDomain(1992), height, xDomain(1992), 0, 0.7, duration, delay);
+    line(svg, xDomain(2012), height, xDomain(2012), 0, 0.7, duration, delay);
+
+    // Horizontal lines
+    position_text(svg, 627, 115, "Automatic qualification for Champions League.", 0, 300);
+    position_text(svg, 627, 135, "Automatic qualification for Europa League.", 0, 300);
+
+    // Annotation using custom annot_line and position_text functions
+    annot_line(svg, xDomain(1992) - 5, yDomain(1) - 5, xDomain(1990), yDomain(-1), duration, delay);
+    position_text(svg, xDomain(1985), yDomain(-1), "Sir Alex Ferguson ", duration, delay);
+    position_text(svg, xDomain(1985), yDomain(-1) + 15, "takes over as Manager", duration, delay);
+    annot_line(svg, xDomain(2012), yDomain(1) - 5, xDomain(2010), yDomain(-1), duration, delay);
+    position_text(svg, xDomain(2006), yDomain(-1), "Sir Alex Ferguson", duration, delay);
+    position_text(svg, xDomain(2006), yDomain(-1) + 15, "resigns as Manager", duration, delay);
+
+    // Conclusion
+    position_text(svg, xDomain(1995), 310, "Sir Alex Ferguson was manager for Manchester United ", duration, delay + 300);
+    position_text(svg, xDomain(1995), 325, "for 21 years (1992 - 2013) and during this tenure Manchester United", duration, delay + 300);
+    position_text(svg, xDomain(1995), 340, "won 13 Premier League championships and have qualified", duration, delay + 300);
+    position_text(svg, xDomain(1995), 355, "for the Champions League in every season. ", duration, delay + 300);
+    position_text(svg, xDomain(1999), 375, " --  The Golder Era -- ", duration, delay + 300, "annot_bold");
 }
 
 async function visualization_6(margin, width, height) {
@@ -482,12 +593,14 @@ async function visualization_6(margin, width, height) {
     //legend.append("text").attr("x", initPosition + 615).attr("y", 12).text("Community Shields").attr("alignment-baseline", "middle").attr("class", "message_text_small");
 };
 
-function position_text(svg, x, y, text, duration, delay) {
+function position_text(svg, x, y, text, duration, delay, cssClass = "annot") {
     svg.append("text")
         .attr("x", x)
         .attr("y", y)
+        .attr("opacity", "0")
         .transition().duration(duration).delay(delay)
-        .attr("class", "annot")
+        .attr("class", cssClass)
+        .attr("opacity", "1")
         .text(text);
 }
 
@@ -522,7 +635,7 @@ function annot_line(svg, x1, y1, x2, y2, duration, delay) {
         .attr("stroke", "black")
         .style("stroke-width", 0.7)
         .style("fill", "none")
-        .attr("transform", "translate(12,0)");;
+        .attr("transform", "translate(12,0)");
 }
 
 function line_plot(svg, data, duration, delay, height, xDomain, yDomain) {
@@ -573,7 +686,8 @@ function dot_plot(svg, data, duration, delay, height, xDomain, yDomain) {
                         + "<br/><font size='-1'><strong>Premier League Champions"
                         + "</strong></font><br/><strong>League Position: </strong>" + d.pos
                         + "</strong></font><br/><strong>Points: </strong>" + d.points
-                        + "<br/><strong>Goals (GF - GA): </strong>" + d.GF + " - " + d.GA);
+                        + "<br/><strong>Goals (GF - GA): </strong>" + d.GF + " - " + d.GA
+                        + "<br/><strong>Manager : </strong>" + d.manager + "(" + d.country + ")");
             }
             else if (d.pos > 1 && d.pos < 5) {
                 tooltip.style("opacity", 1)
